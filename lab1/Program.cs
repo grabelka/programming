4c7432edc50b1e3ba425078f296150dfc17bdd1e
Anastasia Grabovska <nastyusha.gr@gmail.com>
2021-10-02 00:14:44 +0300

    .

---
+++ lab1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+
+namespace lab1
+{
+    abstract class Human
+    {
+        public void Speak() {}
+    }
+    class StupidHuman : Human
+    {
+
+    }
+    class NormalHuman : StupidHuman
+    {
+        
+    }
+    class CleverHuman : NormalHuman
+    {
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
